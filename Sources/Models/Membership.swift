2d8528c174fb1b6f0abb73a6528483dd1f867353
Ravil Khusainov <ravil@lattis.io>
2022-12-19 14:55:35 +0200

    Team added to the Membership

--- Sources/Models/Membership.swift
+++ Sources/Models/Membership.swift
@@ -10,17 +10,17 @@ public struct Membership: Codable, Hashable, Equatable, Identifiable {
     public let period: Period
     public let length: Int
     public let createDate: Date
-    public let teamId: UUID
+    public let team: Team
     public let pricing: [Price]
     
-    public init(id: UUID, name: String, visits: Int, period: Period, length: Int, createDate: Date, teamId: UUID, pricing: [Price]) {
+    public init(id: UUID, name: String, visits: Int, period: Period, length: Int, createDate: Date, team: Team, pricing: [Price]) {
         self.id = id
         self.name = name
         self.visits = visits
         self.period = period
         self.length = length
         self.createDate = createDate
-        self.teamId = teamId
+        self.team = team
         self.pricing = pricing
     }
 }
