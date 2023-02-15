5ee1f9bcbc8b312222ec870bff513f079ed65548
Ravil Khusainov <ravil@partof.team>
2023-02-15 12:43:18 +0300

    Event now has duration and finished params

--- Sources/Models/Event.swift
+++ Sources/Models/Event.swift
@@ -7,18 +7,18 @@ public struct Event: Codable, Hashable, Identifiable {
     public let name: String
     public let createDate: Date
     public let startDate: Date
-    public let estimatedEndDate: Date?
-    public let endDate: Date?
+    public let duration: TimeInterval
+    public let finished: Bool
     public let team: Team
     public let memberships: [Membership]
     
-    public init(id: UUID, name: String, createDate: Date, startDate: Date, estimatedEndDate: Date?, endDate: Date?, team: Team, memberships: [Membership]) {
+    public init(id: UUID, name: String, createDate: Date, startDate: Date, duration: TimeInterval, finished: Bool, team: Team, memberships: [Membership]) {
         self.id = id
         self.name = name
         self.createDate = createDate
         self.startDate = startDate
-        self.estimatedEndDate = estimatedEndDate
-        self.endDate = endDate
+        self.duration = duration
+        self.finished = finished
         self.team = team
         self.memberships = memberships
     }
